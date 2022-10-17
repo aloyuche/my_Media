4afbeda305618af7f009d9575d95a8bf1e5c219b
aloyuche <37053321+aloyuche@users.noreply.github.com>
2022-10-17 23:32:38 +0100

    Add files via upload

---
+++ js/chrit.js
@@ -0,0 +1,107 @@
+const musicContent = document.querySelector(".music-container");
+const musicPlay = document.querySelector("#play");
+const musicPrev = document.querySelector("#prev");
+const musicNext = document.querySelector("#next");
+const musicAudio = document.querySelector("#audio");
+const musicProgress = document.querySelector(".progress");
+const progressContainer = document.querySelector(".progress-container");
+const title = document.querySelector("#title");
+const musicCover = document.querySelector("#cover");
+
+// Song titles
+const songs = [
+  "Bob_fitts_-_05._Our_God",
+  "chineke_m_kaa_midweek_praise",
+  "Chinyere_udoma_Well_Loaded",
+  "DonnieM",
+  "DonWilly",
+  "DonnieMI",
+  "B4DThrone",
+  "Chinecherem",
+  "Chinwe_ike",
+];
+
+// Keep Songs track
+let songIndex = 6;
+
+//Initially load song info
+loadSong(songs[songIndex]);
+
+//update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `https://res.cloudinary.com/cheloytec/video/upload/v1665333973/online-music/${song}.mp3`;
+  cover.src = `https://res.cloudinary.com/cheloytec/image/upload/v1665333970/online-shop/${song}.jpg`;
+}
+function playSong() {
+  musicContent.classList.add("play");
+  musicPlay.querySelector("i.fas").classList.remove("fa-play");
+  musicPlay.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+
+function pauseSong() {
+  musicContent.classList.remove("play");
+  musicPlay.querySelector("i.fas").classList.add("fa-play");
+  musicPlay.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+// Event Listeners
+musicPlay.addEventListener("click", () => {
+  const isPlaying = musicContent.classList.contains("play");
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change songs events
+musicPrev.addEventListener("click", prevSong);
+musicNext.addEventListener("click", nextSong);
+
+audio.addEventListener("timeUpdate", updateProgress);
+progressContainer.addEventListener("click", setProgress);
+
+audio.addEventListener("ended", nextSong);
