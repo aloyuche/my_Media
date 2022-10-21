7f631a58ab20f728829d14555d4c65187d6ecfbb
aloyuche <aloynwa@gmail.com>
2022-10-21 13:02:01 +0100

    Media

--- App.js
+++ App.js
@@ -27,6 +27,26 @@ app.use(
     saveUninitialized: false,
   })
 );
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // handle OPTIONS method
+  if ("OPTIONS" == req.method) {
+    return res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(flash());
 // app.use(paginate.middleware(process.env.LIMIT, process.env.MAX_LIMIT))
 
